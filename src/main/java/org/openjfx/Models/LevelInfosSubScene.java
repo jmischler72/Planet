6c45e6f92507ab4e837f8531384ee4e48fa9d7ef
Kyusiii <mbrevot@gmail.com>
2022-03-16 16:11:53 +0100

    level infos ui

--- src/main/java/org/openjfx/Models/LevelInfosSubScene.java
+++ src/main/java/org/openjfx/Models/LevelInfosSubScene.java
@@ -10,8 +10,8 @@ import java.util.Properties;
 public class LevelInfosSubScene extends SubScene {
 
     private static Properties properties;
-    private static final int WIDHT = 100;
-    private static final int HEIGHT = 100;
+    private static final int WIDHT = 200;
+    private static final int HEIGHT = 200;
 
     public LevelInfosSubScene() {
         super(new AnchorPane(), WIDHT, HEIGHT);
@@ -19,13 +19,23 @@ public class LevelInfosSubScene extends SubScene {
         setLayoutY(this.getLayoutY() + 60);
 
         BackgroundImage background = new BackgroundImage(
-                new Image(getClass().getResource("lvl_button_pressed.png").toExternalForm(), WIDHT, HEIGHT, false, true),
+                new Image(getClass().getResource("Infos_subscene_assets/infos_subscene_background.png").toExternalForm(), WIDHT, HEIGHT, false, true),
                 BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, null);
 
         AnchorPane infosRoot = (AnchorPane) this.getRoot();
         infosRoot.setBackground(new Background(background));
 
-        Button b = new Button("Fight !");
+        Button b = new Button();
+        b.setPrefWidth(80);
+        b.setPrefHeight(30);
+        b.setLayoutX(WIDHT/2 - b.getPrefWidth()/2);
+        b.setLayoutY(HEIGHT - 70);
+
+        b.setBackground(new Background(
+                new BackgroundImage(
+                        new Image(getClass().getResource("Infos_subscene_assets/fight_button.png").toExternalForm(), 80, 30, false, true),
+                        BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, null)
+        ));
         ((AnchorPane) this.getRoot()).getChildren().add(b);
     }
 }
