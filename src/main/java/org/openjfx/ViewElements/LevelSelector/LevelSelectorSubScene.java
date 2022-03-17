8c0a201437cf59fc51f24e615e5b4beb2fcd162c
Kyusiii <mbrevot@gmail.com>
2022-03-17 09:40:46 +0100

    level infos ui

--- src/main/java/org/openjfx/ViewElements/LevelSelector/LevelSelectorSubScene.java
+++ src/main/java/org/openjfx/ViewElements/LevelSelector/LevelSelectorSubScene.java
@@ -1,7 +1,6 @@
 package org.openjfx.ViewElements.LevelSelector;
 
 import javafx.animation.FadeTransition;
-import javafx.scene.Node;
 import javafx.scene.SubScene;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
@@ -22,37 +21,48 @@ public class LevelSelectorSubScene extends SubScene {
     public LevelSelectorSubScene(int buttonSize, Level level) {
         /*size*/
         super(new AnchorPane(), WIDTH, HEIGHT);
-
-        /*positions*/
-        setLayoutX(level.getPosition()[0]);
-        setLayoutY(level.getPosition()[1]);
-
-        this.setFill(Color.BLUE);
         this.setOpacity(0);
         this.setDisable(true);
 
 
-//        BackgroundImage background = new BackgroundImage(
-//                new Image(getClass().getResource("green_planet.png").toExternalForm(), 400, 250, false, true),
-//                BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, null);
-//        this.getPane().setBackground(new Background(background));
+        BackgroundImage background = new BackgroundImage(
+                new Image(getClass().getResource("Infos_subscene_assets/infos_subscene_background.png").toExternalForm(), WIDTH, HEIGHT, false, true),
+                BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, null);
+
+        AnchorPane infosRoot = (AnchorPane) this.getRoot();
+        infosRoot.setBackground(new Background(background));
+
+        Button b = new Button();
+        b.setPrefWidth(80);
+        b.setPrefHeight(30);
+        b.setLayoutX(WIDTH/2 - b.getPrefWidth()/2);
+        b.setLayoutY(HEIGHT - 70);
+
+        b.setBackground(new Background(
+                new BackgroundImage(
+                        new Image(getClass().getResource("Infos_subscene_assets/fight_button.png").toExternalForm(), 80, 30, false, true),
+                        BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, null)
+        ));
+        ((AnchorPane) this.getRoot()).getChildren().add(b);
 
 
         Text text = new Text(100, 50, level.getName());
         text.setFill(Color.NAVAJOWHITE);
+        ((AnchorPane) this.getRoot()).getChildren().add(text);
 
-        Rectangle cadre = new Rectangle(200,20);
+        /*
+        Rectangle agent = new Rectangle(200,10);
 
         StackPane stack = new StackPane();
-        stack.getChildren().addAll(cadre, text);
+        stack.getChildren().addAll(agent, text);
 
-        Button play = new Button("Play");
-        System.out.println(play.getWidth());
-        play.setLayoutY(HEIGHT-35);
 
-        addElement(stack);
-        addElement(play);
+        this.getPane().getChildren().add(stack);
+         */
 
+        /*positions*/
+        setLayoutX(level.getPosition()[0] + 30);
+        setLayoutY(level.getPosition()[1] - 30);
 
     }
 
@@ -74,9 +84,8 @@ public class LevelSelectorSubScene extends SubScene {
         fade.play();
     }
 
-    public void addElement(Node element) {
-        AnchorPane root = (AnchorPane) this.getRoot();
-        root.getChildren().add(element);
+    public AnchorPane getPane() {
+        return (AnchorPane) this.getRoot();
     }
 
 }
