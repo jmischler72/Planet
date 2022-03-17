5a0ce78a7ef1264afc39448ce3eabc6d3acfbe79
Joffrey <Joffrey@DESKTOP-Q8KH83L>
2022-03-17 16:02:39 +0100

    ÃMerge button martin

--- src/main/java/org/openjfx/ViewElements/LevelSelector/LevelSelectorSubScene.java
+++ src/main/java/org/openjfx/ViewElements/LevelSelector/LevelSelectorSubScene.java
@@ -45,13 +45,32 @@ public class LevelSelectorSubScene extends SubScene {
 
         StackPane stack = new StackPane();
         stack.getChildren().addAll(cadre, text);
+//
+//        Button play = new Button("Play");
+//        System.out.println(play.getWidth());
+//        play.setLayoutY(HEIGHT-35);
 
-        Button play = new Button("Play");
-        System.out.println(play.getWidth());
-        play.setLayoutY(HEIGHT-35);
+        BackgroundImage background = new BackgroundImage(
+                new Image(getClass().getResource("Infos_subscene_assets/infos_subscene_background.png").toExternalForm(), WIDTH, HEIGHT, false, true),
+                BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, null);
+
+        AnchorPane infosRoot = (AnchorPane) this.getRoot();
+        infosRoot.setBackground(new Background(background));
+
+        Button b = new Button();
+        b.setPrefWidth(80);
+        b.setPrefHeight(30);
+        b.setLayoutX(WIDTH/2 - b.getPrefWidth()/2);
+        b.setLayoutY(HEIGHT - 70);
+
+        b.setBackground(new Background(
+                new BackgroundImage(
+                        new Image(getClass().getResource("Infos_subscene_assets/fight_button.png").toExternalForm(), 80, 30, false, true),
+                        BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT, null)
+        ));
 
         addElement(stack);
-        addElement(play);
+        addElement(b);
 
 
     }
