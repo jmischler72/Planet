62c6083c87cd5ed47f5b42327991be501e9a811a
jmischler <joffrey.mischler@tiime.fr>
2022-06-08 22:29:41 +0200

    merge last shop + trad attributes

--- src/main/java/org/openjfx/Models/Character/Character.java
+++ src/main/java/org/openjfx/Models/Character/Character.java
@@ -1,7 +1,53 @@
 package org.openjfx.Models.Character;
 
-public class Character {
+public abstract class Personage {
+    private int vie;
+    private int armure;
+    private int degat;
+    private String nom;
+    private double esquive;
 
-    private int health;
-    private int
+    public Personage(int vie, int armure, int degat, String nom, double esquive) {
+        this.vie = vie;
+        this.armure = armure;
+        this.degat = degat;
+        this.nom = nom;
+        this.esquive = esquive;
+    }
+
+    public int getVie() {
+        return vie;
+    }
+
+    public void setVie(int vie) {
+        this.vie = vie;
+    }
+
+    public String getNom() {
+        return nom;
+    }
+
+    public double getEsquive() {
+        return esquive;
+    }
+
+    public void setEsquive(double esquive) {
+        this.esquive = esquive;
+    }
+
+    public int getArmure() {
+        return armure;
+    }
+
+    public int getDegat() {
+        return degat;
+    }
+
+    public void setArmure(int armure) {
+        this.armure = armure;
+    }
+
+    public void setDegat(int degat) {
+        this.degat = degat;
+    }
 }
--- src/main/java/org/openjfx/Models/Character/Player.java
+++ src/main/java/org/openjfx/Models/Character/Player.java
@@ -1,12 +1,64 @@
-package org.openjfx.Models.Character;
+package org.openjfx.Models.Personage;
 
-public class Player {
-    int damage = 0;
-    public Player(){
-        this.damage = 100;
-    }
-    public int getDamage(){
-        return damage;
+import org.openjfx.Models.Character.Personage;
+import org.openjfx.Models.Shop.Armor;
+import org.openjfx.Models.Shop.Item;
+import org.openjfx.Models.Shop.ItemType;
+import org.openjfx.Models.Shop.Weapon;
+
+import java.util.ArrayList;
+
+public class Player extends Personage {
+    private int gold;
+    private ArrayList<Item> items;
+
+    //Stats
+    private double critique;
+    private int ressource;
+
+    public Player(String nom) {
+        super(100, 10, 10, nom, 0);
+        this.critique = 0;
+        this.ressource = 100;
+        this.gold = 0;
+        items = new ArrayList<>();
     }
 
+    public Boolean buy(Item item) { //PlayerController
+        if (item.getCost() > this.gold) return false;
+        this.gold -= item.getCost();
+        this.items.add(item);
+
+        this.setVie(this.getVie() + (int) item.getPv());
+        this.ressource += (int) item.getResource();
+
+        if (item.getType() == ItemType.Weapon) {
+            this.setDegat(getDegat() + (int) ((Weapon) item).getDamage());
+            this.critique += (int) ((Weapon) item).getCritique();
+        } else {
+            this.setArmure(getArmure() + (int) ((Armor) item).getArmor());
+            this.setEsquive(getEsquive() + (int) ((Armor) item).getEsquive());
+        }
+        return true;
+    }
+
+    public void addGolds(int amount) {
+        gold += amount;
+    }
+
+    public ArrayList<Item> getItems() {
+        return items;
+    }
+
+    public int getGold() {
+        return gold;
+    }
+
+    public int getRessource() {
+        return ressource;
+    }
+
+    public double getCritique() {
+        return critique;
+    }
 }
